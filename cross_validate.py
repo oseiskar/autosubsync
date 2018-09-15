a9521523660bdef964032c36fb579bba05298f17
Otto Seiskari <otto.seiskari@gmail.com>
2018-09-15 11:23:24 +0300

    Repeated random cross validation

--- cross_validate.py
+++ cross_validate.py
@@ -5,10 +5,15 @@ import find_transform
 import quality_of_fit
 import model
 
+np.random.seed(1)
+
 def cv_split_by_file(data_meta, data_x):
     files = np.unique(data_meta.file_number)
+    np.random.shuffle(files)
+
     n_train = int(round(len(files)*0.5))
     train_files = files[:n_train]
+    print(train_files)
 
     train_cols = data_meta.file_number.isin(train_files)
     test_cols = ~train_cols
@@ -70,25 +75,32 @@ def test_quality_of_fit_mismatch(result_meta):
     return np.array(qualities)
 
 if __name__ == '__main__':
-    print('## Training phase')
 
     data_x = np.load('training-data/features.npy')
     print('loaded training features of size', data_x.shape)
     data_meta = pd.read_csv('training-data/meta.csv', index_col=0)
+    n_folds = 4
 
-    train_meta, train_x, test_meta, test_x = cv_split_by_file(data_meta, data_x)
-    trained_model = model.train(train_x, train_meta.label, train_meta.file_number)
-    print('model trained')
+    correct_qualities = []
 
-    print('## Validation phase')
-    predicted_score = model.predict(trained_model, test_x, test_meta.file_number)
-    result_meta = test_meta.assign(predicted_score=predicted_score)
-    result_meta = result_meta.assign(predicted_label=np.round(predicted_score))
-    result_meta = result_meta.assign(label=np.round(result_meta.label))
-    result_meta = result_meta.assign(correct=result_meta.predicted_label==result_meta.label)
+    for i in range(n_folds):
+        print('### Cross-validation fold %d/%d' % (i+1, n_folds))
+        train_meta, train_x, test_meta, test_x = cv_split_by_file(data_meta, data_x)
 
-    validate_speech_detection(result_meta)
-    correct_qualities = test_correct_sync(result_meta)
+        print('Training...', train_x.shape)
+        trained_model = model.train(train_x, train_meta.label, train_meta.file_number)
+
+        print('Validating...')
+        predicted_score = model.predict(trained_model, test_x, test_meta.file_number)
+        result_meta = test_meta.assign(predicted_score=predicted_score)
+        result_meta = result_meta.assign(predicted_label=np.round(predicted_score))
+        result_meta = result_meta.assign(label=np.round(result_meta.label))
+        result_meta = result_meta.assign(correct=result_meta.predicted_label==result_meta.label)
+
+        validate_speech_detection(result_meta)
+        correct_qualities.extend(test_correct_sync(result_meta))
+
+    print('### Quailty of fit mismatch test (with last fold)')
     mismatch_qualities = test_quality_of_fit_mismatch(result_meta)
 
     min_correct = np.min(correct_qualities)
