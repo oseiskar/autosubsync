f9fee0baa67766ff82c293915c87402abae39dcb
Otto Seiskari <otto.seiskari@gmail.com>
2018-10-03 23:00:20 +0300

    Fixed simultaneous Python 2 and 3 distribution

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ package_name = 'autosubsync'
 
 setup(
     name=package_name,
-    version='0.1.2',
+    version='0.1.3',
     description='Automatically synchronize subtitles with audio',
     long_description=long_description,
     url='https://github.com/oseiskar/' + package_name,
